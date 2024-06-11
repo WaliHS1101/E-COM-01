dbc1c5be94b2664a41d8a900682ced465624d2ef
WaliHS1101 <walih1101@gmail.com>
2024-06-11 13:15:52 +0500

    Add Products page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import TopBar from './Components/TopBar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Components/Home'
+import Products from './Components/Products'
 
 function App() {
   return (
     <>
-    <TopBar/>
-    <Navbar/>
+      <TopBar />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='Products' element={<Products />} />
+      </Routes>
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,15 +5,15 @@ function Navbar() {
     return (
         <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
             <div className='container'>
-                <span className='navbar-brand'>Your Brand</span>
-                <span className='navbar-brand d-block d-lg-none'> <img src={navlogo} width="20px" alt="Logo" /></span>
+                <span className='navbar-brand'><a href="/" className='nav-link text-white'> Your Brand</a></span>
+                <span className='navbar-brand d-block d-lg-none'><a href="/">  <img src={navlogo} width="20px" alt="Logo" /></a></span>
                 <button className='navbar-toggler' data-bs-toggle="collapse" data-bs-target="#Nav-Bar">
                     <span className='navbar-toggler-icon'></span>
                 </button>
                 <div className='collapse navbar-collapse' id="Nav-Bar">
                     <ul className='nav navbar-nav'>
                         <li className='nav-item mt-lg-0 mb-lg-0 m-lg-4'>
-                            <a href="#" className='nav-link'>Home</a>
+                            <a href="Products" className='nav-link'>Products</a>
                         </li>
                         <li className='nav-item mt-lg-0 mb-lg-0 m-lg-4'>
                             <a href="#" className='nav-link'>Home</a>
@@ -23,7 +23,7 @@ function Navbar() {
                         </li>
                     </ul>
                 </div>
-                <span className='navbar-brand d-none d-lg-block'> <img src={navlogo} width="20px" alt="Logo" /></span>
+                <span className='navbar-brand d-none d-lg-block'><a href="/">  <img src={navlogo} width="20px" alt="Logo" /></a></span>
 
             </div>
         </nav>
